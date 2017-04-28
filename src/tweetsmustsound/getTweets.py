96bce7327fce4b12119b165246fa80d585a0b7e2
Betekhtin <st040186@student.spbu.ru>
2017-04-28 21:01:21 +0300

    english tweets

--- src/tweetsmustsound/getTweets.py
+++ src/tweetsmustsound/getTweets.py
@@ -30,6 +30,15 @@ class TwitMiner:
 
 	def get_tweets(self, username):
 		statuses = api.GetUserTimeline(screen_name = username, count = 200)
+		
+		#iterations = min(tweets[0].user.statuses_count // 200, 16)
+
+		#for i in range(0,iterations):
+		#	lastId = tweets[-1].id
+		#	statuses += api.GetUserTimeline(screen_name = screen_name, 
+		#							  	  count = 200, 
+		#						  	      max_id = lastId)
+		
 		statuses_text = [(s.text) for s in statuses]
 		hashes = [self.get_hashes(s) for s in statuses_text]
 		statuses_text = [self.clean_tweet(s.text) for s in statuses]
