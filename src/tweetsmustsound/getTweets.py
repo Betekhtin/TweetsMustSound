289a0b93830d58c72c3e4cbb1a9715a1a47806f7
inventive-seven <mr.kakorin@yandex.ru>
2017-04-29 00:10:08 +0300

    tw api imp for hashes

--- src/tweetsmustsound/getTweets.py
+++ src/tweetsmustsound/getTweets.py
@@ -27,29 +27,29 @@ class TwitMiner:
 		return nhashes
 
 	def get_tweets(self, username):
+		#statuses = self.api.GetSearch(raw_query = "q=from%3A" + username + "&lang=en&count=200")
 		statuses = self.api.GetUserTimeline(screen_name = username, count = 200)
-		
-		#iterations = min(tweets[0].user.statuses_count // 200, 16)
-
-		#for i in range(0,iterations):
-		#	lastId = tweets[-1].id
-		#	statuses += api.GetUserTimeline(screen_name = screen_name, 
-		#							  	  count = 200, 
-		#						  	      max_id = lastId)
-		
+		iterations = min(statuses[0].user.statuses_count // 200, 16)
+		statuses = [s for s in statuses if '#' in s.text]
+		for i in range(0,iterations):
+			lastId = statuses[-1].id
+			nst = self.api.GetSearch(raw_query = "q=from%3A" + username + "&since_id="+str(lastId)+"&lang=en&count=200")
+			statuses += [s for s in nst if '#' in s.text]
+			if len(statuses)>=200:
+				break
 		statuses_text = [(s.text) for s in statuses]
-		#hashes = list(filter(None,[self.get_hashes(s) for s in statuses_text]))
 		st_hashes = [self.get_hashes(s) for s in statuses_text]
 		statuses_text = [self.clean_tweet(s.text) for s in statuses]
 		hash_stat =[]
 		for h in st_hashes:
-			t=[]
 			for i in h:
+				t=[]
 				if len(i)>0:
-					it_data = self.api.GetSearch(raw_query = "q=twitter%20&count=20&" + i)
+					it_data = self.api.GetSearch(raw_query = "q=twitter%20&count=20&lang=en" +"%20"+i)
 					t = [self.clean_tweet(s.text) for s in it_data]
-			hash_stat.append(t)
-		return statuses_text,hash_stat
+				hash_stat.append(t)
+		return statuses_text, hash_stat
+
 	def eng_tweets(self, statuses):
 		result = []
 		for status in statuses:
@@ -58,5 +58,7 @@ class TwitMiner:
 		return result
 
 uname = "realDonaldTrump"
+
 gtw = TwitMiner()
 tw,htw = gtw.get_tweets(uname)
+print(tw)
