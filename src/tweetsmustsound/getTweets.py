cf426285b3f898840bb515d621b734b3742da42e
inventive-seven <mr.kakorin@yandex.ru>
2017-04-28 20:13:32 +0300

    mine twitter

--- src/tweetsmustsound/getTweets.py
+++ src/tweetsmustsound/getTweets.py
@@ -1,17 +1,29 @@
 import twitter
 import secrets
+import re
 
-api = twitter.Api(consumer_key = apiKeys.consumer_key,
-				  consumer_secret = apiKeys.consumer_secret,
-				  access_token_key = apiKeys.access_token_key,
-				  access_token_secret = apiKeys.access_token_secret)
+api = twitter.Api(consumer_key = secrets.consumer_key,
+				  consumer_secret = secrets.consumer_secret,
+				  access_token_key = secrets.access_token_key,
+				  access_token_secret = secrets.access_token_secret)
 
-users = api.GetFriends()
 
-#auth = tweepy.OAuthHandler(apiKeys.consumer_key, apiKeys.consumer_secret)
-#auth.set_access_token(apiKeys.access_token_key, apiKeys.access_token_secret)
+class TwitMiner:
+	def __init__(self):
+		self.api = 	twitter.Api(consumer_key = secrets.consumer_key,
+				  consumer_secret = secrets.consumer_secret,
+				  access_token_key = secrets.access_token_key,
+				  access_token_secret = secrets.access_token_secret)
+	def clean_tweet(tweet):
+		return ' '.join(re.sub("(@[A-Za-z0-9]+)|([^0-9A-Za-z \t])|(\w+:\/\/\S+)", " ", tweet).split())
+	def get_hashes(tweet_text):
+		hashes = [s for s in tweet_text.split() if (s[0]=='#')]
+	def get_tweets(self, username):
+		statuses = api.GetUserTimeline(screen_name = username)
+		statuses_text = [self.clean_tweet(s.text) for s in statuses]
+		return statuses_text;
 
-#api = tweepy.API(auth)
-#public_tweets = api.home_timeline()
-#for tweet in public_tweets:
-#    print (tweet.text)
+uname = "realDonaldTrump"
+gtw = TwitMiner
+tw = gtw.get_tweets(gtw,uname)
+print(tw)
