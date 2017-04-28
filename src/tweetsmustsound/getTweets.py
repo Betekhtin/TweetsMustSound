589c5871d320906a9ad020943a88250106f682c1
Betekhtin <st040186@student.spbu.ru>
2017-04-28 16:23:21 +0300

    get tweets

--- src/tweetsmustsound/getTweets.py
+++ src/tweetsmustsound/getTweets.py
@@ -1,17 +1,30 @@
 import twitter
 import secrets
+import re
 
-api = twitter.Api(consumer_key = apiKeys.consumer_key,
-				  consumer_secret = apiKeys.consumer_secret,
-				  access_token_key = apiKeys.access_token_key,
-				  access_token_secret = apiKeys.access_token_secret)
+api = twitter.Api(consumer_key = secrets.consumer_key,
+				  consumer_secret = secrets.consumer_secret,
+				  access_token_key = secrets.access_token_key,
+				  access_token_secret = secrets.access_token_secret)
 
-users = api.GetFriends()
+def clean_tweet(tweet):
+	return ' '.join(re.sub("(@[A-Za-z0-9]+)|([^0-9A-Za-z \t])|(\w+:\/\/\S+)", " ", tweet).split())
 
-#auth = tweepy.OAuthHandler(apiKeys.consumer_key, apiKeys.consumer_secret)
-#auth.set_access_token(apiKeys.access_token_key, apiKeys.access_token_secret)
+def get_tweets(screen_name):
+	tweets = api.GetUserTimeline(screen_name = screen_name, count = 200)
 
-#api = tweepy.API(auth)
-#public_tweets = api.home_timeline()
-#for tweet in public_tweets:
-#    print (tweet.text)
+#	for i in range(0,16):
+#		lastId = tweets[-1].id
+#		tweets += api.GetUserTimeline(screen_name = screen_name, 
+#									  count = 200, 
+#								  max_id = lastId)
+	return tweets
+
+def print_tweets(tweets):
+	i = 0
+	for tweet in tweets:
+		print(str(i) + ":" + tweet.text)
+		i += 1
+
+tweets = get_tweets("AsWhichOneIAm")
+print_tweets(tweets)
